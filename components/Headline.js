788e9f67972a950ca10af427cc898b135d6f0782
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-12 07:25:29 +0900

    update: propsからchildrenに変更

--- components/Headline.js
+++ components/Headline.js
@@ -6,7 +6,7 @@ export function HeadLine(props) {
             <h1 className={styles.title}>{props.title}</h1>
             <p className={styles.description}>
                 Get started by editing{' '}
-                <code className={styles.code}>pages/{props.page}</code>
+                {props.children}
             </p>
         </div>
     )
