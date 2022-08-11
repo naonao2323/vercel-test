52cbe242e04b8bdca18d6adfc9a49372468ca6b7
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-12 08:15:47 +0900

    update: css module化

--- components/Main.js
+++ components/Main.js
@@ -1,12 +1,12 @@
-import styles from '../styles/Home.module.css'
+import classes from './Main.module.css'
 import { HeadLine }  from '../components/Headline'
 import { Links }  from '../components/Links'
 
 export function Main(props) {
     return (
-        <main className={styles.main}>
+        <main className={classes.main}>
         <HeadLine title="About Page" page={props.page}> 
-          <code className={styles.code}>pages/{props.page}.js</code>
+          <code className={classes.code}>pages/{props.page}.js</code>
         </HeadLine>     
         <Links />
       </main>
