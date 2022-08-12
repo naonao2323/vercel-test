8c303a4a98706ca9ff6e3f950ae983de1bd1c55b
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-13 04:05:19 +0900

    add: ライフサイクルチェック

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from "../components/Header"
-import { useCallback } from 'react'
+import { useCallback, useEffect } from 'react'
 
 
 export default function Home() {
@@ -11,6 +11,16 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     alert(foo);
+  }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
   }, []);
 
   return (
