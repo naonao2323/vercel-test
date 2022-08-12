b1dd94ddd800e2539147b7e52360b9f030478bd0
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-13 03:38:51 +0900

    add: onclickイベント

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,22 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from "../components/Header"
+import { useCallback } from 'react'
+
 
 export default function Home() {
+  const foo = 123;
+
+  const handleClick = useCallback(() => {
+    alert(foo);
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
       </Head>
+      <a href="/about"><button onClick={handleClick}>ボタン</button></a>
       <Header />
       <Main page="index"/>
       <Footer />
