563cd83faa62559ae640021108d1739d67e4ab01
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-13 04:48:22 +0900

    update: カウントアップ機能

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,22 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from "../components/Header"
-import { useCallback, useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 
 export default function Home() {
-  const foo = 123;
-
-  const handleClick = useCallback(() => {
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1);
+  const handleClick = () => {
+    setCount(count =>(count+1));
+    setCount(count =>(count+1));
+  };
 
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
 
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
@@ -29,8 +26,11 @@ export default function Home() {
       <Head>
         <title>Create Next App</title>
       </Head>
-      <Link href="/about"><a><button onClick={handleClick}>ボタン</button></a></Link>
       <Header />
+      <div className={styles.countSpace}>
+        <p>{count}</p>
+        <button onClick={handleClick}>ボタン</button>
+      </div>
       <Main page="index"/>
       <Footer />
     </div>
