290ee90393747407692d85da03e689abf891a43a
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-13 15:16:11 +0900

    update: handleClick

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,15 +3,16 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from "../components/Header"
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 
 
 export default function Home() {
   const [count, setCount] = useState(1);
-  const handleClick = () => {
-    setCount(count =>(count+1));
-    setCount(count =>(count+1));
-  };
+  const handleClick = useCallback(() => {
+    if(count < 10) {
+      setCount(count =>(count+1));
+    }
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -19,7 +20,7 @@ export default function Home() {
     return () => {
       document.body.style.backgroundColor = "";
     };
-  }, []);
+  }, [count]);
 
   return (
     <div className={styles.container}>
