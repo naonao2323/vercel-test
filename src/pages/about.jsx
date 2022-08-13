eb2a15d1b64ca804d56c292ea4858847e31c219e
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-14 06:58:56 +0900

    update: ページ間で状態を共通化

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,14 +3,9 @@ import { Main } from 'src/components/Main'
 import { Footer } from 'src/components/Footer'
 import { Header } from 'src/components/Header'
 import styles from 'src/styles/Home.module.css'
-import { useCounter } from 'src/hooks/useCounter'
-import { useInputArray } from 'src/hooks/useInputArray'
-import { useBgLightblue } from 'src/hooks/useBgLightblue'
 
-export default function About() {
-  const {count, isShow, handleClick, handleDisplay} = useCounter();
-  const {text, array, handleChange, handleAdd } = useInputArray();
-  useBgLightblue();
+export default function About(props) {
+  const {count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd} = props;
 
   return (
     <div className={styles.container}>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,14 +3,9 @@ import styles from '../styles/Home.module.css'
 import { Footer } from '../components/Footer'
 import { Main } from '../components/Main'
 import { Header } from "../components/Header"
-import { useCounter } from 'src/hooks/useCounter'
-import { useInputArray } from 'src/hooks/useInputArray'
-import { useBgLightblue } from 'src/hooks/useBgLightblue'
 
-export default function Home() {
-  const {count, isShow, handleClick, handleDisplay} = useCounter();
-  const {text, array, handleChange, handleAdd } = useInputArray();
-  useBgLightblue();
+export default function Home(props) {
+  const {count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd} = props;
 
   return (
     <div className={styles.container}>
