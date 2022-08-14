8e30210e5895ad6ee7430ecaee3743130f8370b1
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-14 23:41:01 +0900

    update: api叩く

---
+++ src/components/Posts/index.jsx
@@ -0,0 +1,38 @@
+import { useState, useCallback, useEffect } from 'react';
+
+export const Posts = () => {
+    const [posts, setPosts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const getPosts = useCallback(async () => {
+        try {
+            const res = await fetch("http://jsonplaceholder.typicode.com/posts");
+            if (!res.ok) {
+                throw new Error("データが発生したため、データ取得できませんでした。")
+            }
+            const json = await res.json();
+            setPosts(posts => posts = json);
+        } catch (error) {
+            setError(error);
+        }
+        setLoading(false);
+    }, []);
+
+    useEffect(() => {
+        getPosts();
+    }, [getPosts]);
+
+    if (loading) return (<div>loading中です</div>);
+    if (error) return (<div>{error.message}</div>);
+    if (posts.length <= 0) return (<div>データは空です。</div>);
+    return (
+        <ol>
+            {posts.map((post, x) => {
+                return (
+                    <li key={x}>{post.title}</li>
+                )
+            })}
+        </ol>
+    )
+}
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,9 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import { Footer } from '../components/Footer'
-import { Main } from '../components/Main'
-import { Header } from "../components/Header"
+import { Header } from '../components/Header'
+import { Posts } from 'src/components/Posts';
 
-
-const Home = (props) => {
-  const {count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd} = props;
+const Home = () => {
 
   return (
     <div className={styles.container}>
@@ -14,20 +11,7 @@ const Home = (props) => {
         <title>Create Next App</title>
       </Head>
       <Header />
-      <div className={styles.countSpace}>
-        {isShow ? <p>{count}</p>: null}
-        <button onClick={handleClick}>ボタン</button>
-        <input type="text" value={text} onChange={handleChange} />
-        <button onClick={handleDisplay}>{isShow ?　"非表示" : "表示"}</button>
-        <button onClick={handleAdd}>追加</button>
-        <ul>
-          {array.map((item, i) => {
-            return <li key={i}>{item}</li>
-          })}
-        </ul>
-      </div>
-      <Main page="index"/>
-      <Footer />
+      <Posts />
     </div>
   )
 }
