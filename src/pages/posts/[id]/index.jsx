70c24eefae8399c439a0c52414818abb26d120fc
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-16 18:12:44 +0900

    delete: 不要なconsoleとhooks削除

--- src/pages/posts/[id]/index.jsx
+++ src/pages/posts/[id]/index.jsx
@@ -1,15 +1,8 @@
 import styles from 'src/styles/Home.module.css'
 import { Header } from 'src/components/Header'
-import { usePost } from 'src/hooks/usePost';
-import { Post } from 'src/components/Post';
+import { Post } from 'src/components/Post'
 
 const Posts = () => {
-    const { post, user, error, isLoading } = usePost();
-    console.log({ post, user, error, isLoading });
-
-
-    if (isLoading) return (<div>loading中です</div>);
-    if (error) return (<div>{error.message}</div>);
 
     return (
         <div className={styles.container}>
