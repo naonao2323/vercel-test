3711f9c971bd631331f7d36a8a0e3a92931ddbf0
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-17 16:13:42 +0900

    update: ヘッダーの下に余白を追加

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -9,7 +9,7 @@ const NAV_ITEMS = [
 
 export const Header = () => {
   return (
-    <header className={"flex items-center justify-center border-b w-full h-24"}>
+    <header className={"flex items-center justify-center border-b w-full h-24 mb-4"}>
       {NAV_ITEMS.map((item, x) => {
         return (
           <Link key={x} href={item.href}><a className={"inline-block py-1 px-3 text-lg hover:text-blue-500 focus:text-blue-500 active:text-blue-500"}>{item.label}</a></Link>
