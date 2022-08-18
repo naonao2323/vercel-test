20d50c3a478285ace85acf1684078f460763cd3a
naonao <naonao@naonaonoMacBook-Air.local>
2022-08-18 16:52:41 +0900

    update: ssr化

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,16 +1,36 @@
-import Head from 'next/head'
 import { Header } from 'src/components/Header'
 import { User as UserComponent } from 'src/components/User';
+import { SWRConfig } from 'swr';
 
-const User = () => {
+export const getServerSideProps = async (ctx) => {
+    const { id } = ctx.query;
+    const USER_API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+    const user = await fetch(USER_API_URL);
+    const userData = await user.json();
+
+    const POST_API_URL = `https://jsonplaceholder.typicode.com/posts?userId=${userData.id}`;
+    const post = await fetch(POST_API_URL);
+    const postData = await post.json();
+
+    return {
+        props: {
+            fallback: {
+                [USER_API_URL]: userData,
+                [POST_API_URL]: postData,
+            }
+        }
+    }
+}
+
+const User = (props) => {
+    const { fallback } = props;
 
     return (
         <div>
-            <Head>
-                <title>Create Next App</title>
-            </Head>
-            <Header />
-            <UserComponent />
+            <SWRConfig value={{ fallback }} >
+                <Header />
+                <UserComponent />
+            </SWRConfig>
         </div>
     )
 }
